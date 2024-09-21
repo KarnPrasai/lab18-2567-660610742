a5afff0f4d79cccd74d0ac1207b8937d92348f79
KarnPrasai <karn_prasai@cmu.ac.th>
2024-09-21 17:12:28 +0700

    finish

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   return NextResponse.json({
     ok: true,
-    fullName: "Dome Potikanond",
-    studentId: "660610999",
+    fullName: "Karn Prasai",
+    studentId: "660610742",
   });
 };
